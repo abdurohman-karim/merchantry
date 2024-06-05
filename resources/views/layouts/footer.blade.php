792a7cde7f59fb257de197e834b481d135e46538
abdurohman-karim <ghostmagic766@gmail.com>
2024-06-05 10:15:58 +0500

    footer updated

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,13 +2,13 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Skote.
+                <script>document.write(new Date().getFullYear())</script> © GMC.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Vedo Group
-                    <a href="https://github.com/shakhzodbek96" class="mx-1"><i class="bx bxl-github font-size-18"></i></a>
-                    <a href="https://t.me/+F08XSd5G7yFiZTky" class="mx-1"><i class="bx bxl-telegram font-size-18"></i></a>
+                    Design & Develop by GMC
+                    <a href="https://github.com/abdurohman-karim" class="mx-1"><i class="bx bxl-github font-size-18"></i></a>
+                    <a href="https://t.me/abdurohman_karimov" class="mx-1"><i class="bx bxl-telegram font-size-18"></i></a>
                 </div>
             </div>
         </div>
